18c94fd388624940360e5ed5ee4f888656b0ee33
VivekG11 <vivekvk2903@gmail.com>
2021-07-23 21:55:18 +0530

    [Initial Commit]

---
+++ Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Collections Programs......");
+            //Operations.ListFunction();
+            //Operations.StackFunction();
+            // Operations.QueueFunction();
+            //Operations.DictionaryFunction();
+            Operations.SetMethod();
+        }
+
+       
+    }
+       
+}
+        
+
+        
+
+
